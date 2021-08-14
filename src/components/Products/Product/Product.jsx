6937110179fbc64074ea653314c26d2121402605
viktoriialeseiko <vikyshaaa1@gmail.com>
2021-08-13 20:04:12 -0500

    initial commit

---
+++ src/components/Products/Product/Product.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Card, CardMedia, CardContent, CardActions, Typography, IconButton } from '@material-ui/core';
+import { AddShoppingCart } from '@material-ui/icons';
+
+const Product = () => {
+
+  return (
+    <Card className={classes.root}>
+      <CardMedia className={classes.media} image='' title={product.name} />
+      <CardContent>
+        <div className={classes.cardContent}>
+          <Typography gutterBottom variant="h5" component="h2">
+            {product.name}
+          </Typography>
+          <Typography gutterBottom variant="h5" component="h2">
+            ${product.price.formatted}
+          </Typography>
+        </div>
+        <Typography dangerouslySetInnerHTML={{ __html: product.description }} variant="body2" color="textSecondary" component="p" />
+      </CardContent>
+      <CardActions disableSpacing className={classes.cardActions}>
+        <IconButton aria-label="Add to Cart" onClick={handleAddToCart}>
+          <AddShoppingCart />
+        </IconButton>
+      </CardActions>
+    </Card>
+  );
+};
+
+export default Product;
+
---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import Product from './Product/Product';
+
+const products = [
+    {id:1, name: "Shoes", description: "Running shoes."},
+    {id:2, name: "MacBook", description: "Apple MacBook."},
+];
+
+const Products = () => {
+
+  return (
+    <main>
+      <Grid container justify="center" spacing={4}>
+      {products.map((product) => (
+          <Grid key={product.id} xs={12} sm={6} md={4} lg={3}>
+            <Product  product={product} />
+          </Grid>
+        ))}
+      </Grid>
+    </main>
+  );
+};
+
+export default Products;
+
